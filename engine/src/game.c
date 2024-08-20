93e02cf646861db1ac4ed5aede1c28b2033a4585
Michael Harris <mharris@luabase.com>
2024-08-20 15:36:00 -0400

    encapsulate ops for better readability

--- engine/src/game.c
+++ engine/src/game.c
@@ -4,6 +4,29 @@ unsigned int MOVE_COUNT    = 0x3C0000;
 unsigned int PLAYER_CHOICE = 0x400000;
 unsigned int TURN_TAKER    = 0x800000;
 
+// Private
+void reset_move_count(State *state) { *state &= ~MOVE_COUNT; }
+
+void set_move_count(State *state, unsigned short n) {
+  reset_move_count(state);
+  *state |= n << 18;
+}
+
+void set_square_state(State *state, unsigned short square,
+                      unsigned short turn_taker) {
+  unsigned int shift = square << 1;
+  *state |= 2 << shift;
+  *state |= shift == 0 ? turn_taker : turn_taker << (shift - 1);
+}
+
+unsigned int is_square_set(State *state, unsigned short square) {
+  unsigned int shift = square << 1;
+  return *state & 2 << shift;
+}
+
+void toggle_turn_taker(State *state) { *state ^= TURN_TAKER; }
+
+// Public
 State new_game(unsigned short player_side) {
   return (2 | (player_side & 0x1)) << 22;
 }
@@ -17,27 +40,21 @@ Move encode_move(unsigned short side, unsigned short square) {
 }
 
 int transition(State *state, Move *move) {
-  unsigned short expected_turn = (*state & TURN_TAKER) >> 23;
-  unsigned short actual_turn   = *move >> 4;
+  unsigned short turn_taker = *move >> 4;
+  unsigned int square       = *move & 0xF;
+  unsigned int move_count   = get_move_count(state);
 
-  if (expected_turn != actual_turn) {
+  if (get_turn_taker(state) != turn_taker) {
     return -1;
   }
 
-  unsigned int shift = 2 * (*move & 0xF);
-
-  if ((*state & (2 << shift)) == 2) {
+  if (is_square_set(state, square) == 1) {
     return 0;
   }
 
-  unsigned int n = (*state & MOVE_COUNT) >> 18;
-  *state &= ~MOVE_COUNT;
-  *state |= (n + 1) << 18;
-
-  *state |= 2 << shift;
-  *state |= shift == 0 ? actual_turn : actual_turn << (shift - 1);
-
-  *state ^= TURN_TAKER;
+  set_move_count(state, ++move_count);
+  set_square_state(state, square, turn_taker);
+  toggle_turn_taker(state);
 
   return 0;
 }

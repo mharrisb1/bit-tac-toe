2f7a5abd7758039744a8cea329e3b9d68ffa7faa
Michael Harris <mharris@luabase.com>
2024-08-20 13:43:10 -0400

    add more tests

--- engine/tests/test_game.c
+++ engine/tests/test_game.c
@@ -3,21 +3,68 @@
 
 #include "game.h"
 
+void test_get_move_count() {
+  State state;
+  for (unsigned int i = 0; i < 9; i++) {
+    state = MOVE_COUNT & (i << 18);
+    assert(get_move_count(&state) == i);
+  }
+}
+
+void test_get_player_choice() {
+  State s1 = new_game(1);
+  assert(get_player_choice(&s1) == 1);
+
+  State s2 = new_game(0);
+  assert(get_player_choice(&s2) == 0);
+}
+
+void test_get_turn_taker() {
+  State state = 0x800000;
+  assert(get_turn_taker(&state) == 1);
+
+  state = 0x0;
+  assert(get_turn_taker(&state) == 0);
+}
+
 void test_new_game() {
-  assert(new_game(0) == 0x800000);
-  assert(new_game(1) == 0xC00000);
+  State s1 = new_game(1);
+  assert(get_move_count(&s1) == 0);
+  assert(get_player_choice(&s1) == 1);
+  assert(get_turn_taker(&s1) == 1);
+
+  State s2 = new_game(0);
+  assert(get_move_count(&s2) == 0);
+  assert(get_player_choice(&s2) == 0);
+  assert(get_turn_taker(&s2) == 1);
 }
 
 void test_restart_game() {
-  State state = new_game(1);
-  state       = 0x440003;
-  restart_game(&state);
-  assert(state == 0xC00000);
+  State s1 = new_game(1);
+  Move m1  = encode_move(1, 7);
 
-  state = new_game(0);
-  state = 0x40002;
-  restart_game(&state);
-  assert(state == 0x800000);
+  transition(&s1, &m1);
+  assert(get_move_count(&s1) == 1);
+  assert(get_player_choice(&s1) == 1);
+  assert(get_turn_taker(&s1) == 0);
+
+  restart_game(&s1);
+  assert(get_move_count(&s1) == 0);
+  assert(get_player_choice(&s1) == 1);
+  assert(get_turn_taker(&s1) == 1);
+
+  State s2 = new_game(0);
+  Move m2  = encode_move(1, 7);
+
+  transition(&s2, &m2);
+  assert(get_move_count(&s2) == 1);
+  assert(get_player_choice(&s2) == 0);
+  assert(get_turn_taker(&s2) == 0);
+
+  restart_game(&s2);
+  assert(get_move_count(&s1) == 0);
+  assert(get_player_choice(&s2) == 0);
+  assert(get_turn_taker(&s1) == 1);
 }
 
 void test_encode_move() {
@@ -43,12 +90,22 @@ void test_invalid_transition() {
 
 void test_transition() {
   State state = new_game(1);
-  Move move   = encode_move(1, 0);
 
-  transition(&state, &move);
+  Move move = encode_move(1, 0);
+  int t     = transition(&state, &move);
 
-  printf("%x\n", state);
-  assert(state == 0xC40003);
+  assert(t == 0);
+  assert(get_move_count(&state) == 1);
+  assert(get_player_choice(&state) == 1);
+  assert(get_turn_taker(&state) == 0);
+
+  move = encode_move(0, 1);
+  t    = transition(&state, &move);
+
+  assert(t == 0);
+  assert(get_move_count(&state) == 2);
+  assert(get_player_choice(&state) == 1);
+  assert(get_turn_taker(&state) == 1);
 }
 
 void test_ignore_taken_square() {
@@ -56,13 +113,19 @@ void test_ignore_taken_square() {
   Move move   = encode_move(1, 0);
 
   transition(&state, &move);
-  assert(state == 0xC40003);
+  assert(state == 0x440003);
 
   transition(&state, &move);
-  assert(state == 0xC40003);
+  assert(state == 0x440003);
+
+  assert(1 == 0); // FIXME: this should fail bc of unexpected turn taker
 }
 
 int main() {
+  test_get_move_count();
+  test_get_player_choice();
+  test_get_turn_taker();
+
   test_new_game();
   test_restart_game();
   test_encode_move();
